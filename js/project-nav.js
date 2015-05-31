003171bf5bfd3f71981643b470afa21bbc20e65f
mixflow <wangweimf@gmail.com>
2015-05-31 14:18:23 +0800

    center body in wide screen

--- js/project-nav.js
+++ js/project-nav.js
@@ -3,8 +3,8 @@ function generateNav(){
 
 	var nav = document.createElement("nav");
 
-	nav.innerHTML = '<a href="only-html5-video.html">OnlyHTML5Video</a> \
-	<a href="html5-games.html">HTML5 Games</a> \
+	nav.innerHTML = '<a class="nav-link" href="only-html5-video.html">OnlyHTML5Video</a> \
+	<a class="nav-link" href="html5-games.html">HTML5 Games</a> \
 	';
 
 	var bodyEl = document.querySelector("body");
